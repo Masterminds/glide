e82542e456a8f5dee62ede0be1b861c2a7bc1276
Matt Farina <matt@mattfarina.com>
2014-09-25 21:12:36 -0400

    Commenting out the Out command as you need to 'exit' the shell.

--- glide.go
+++ glide.go
@@ -216,8 +216,8 @@ func routes(reg *cookoo.Registry, cxt cookoo.Context) {
 		Includes("@startup").
 		Does(cmd.In, "gopath")
 
-	reg.Route("out", "Set GOPATH back to former val.").
-		Does(cmd.Out, "gopath")
+	// reg.Route("out", "Set GOPATH back to former val.").
+	// 	Does(cmd.Out, "gopath")
 
 	reg.Route("install", "Install dependencies.").
 		Includes("@startup").
