06019bbe2260d92530af45d81001a5b9f8aac010
Matt Farina <matt@mattfarina.com>
2014-10-03 13:19:05 -0400

    Bumping the internal version for development

--- glide.go
+++ glide.go
@@ -42,7 +42,7 @@ import (
 	"os"
 )
 
-var version string = "0.2.0"
+var version string = "0.3.0-dev"
 
 const Summary = "Manage Go projects with ease."
 const Usage = `Manage dependencies, naming, and GOPATH for your Go projects.
