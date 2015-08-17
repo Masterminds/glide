a14f26c4e64a0542c89a100bd71f2fed469ea0db
Matt Butcher <mattbutcher@google.com>
2015-08-17 06:37:08 -0600

    Updated docs on PrintName.

--- cmd/print_name.go
+++ cmd/print_name.go
@@ -6,6 +6,9 @@ import (
 	"github.com/Masterminds/cookoo"
 )
 
+// PrintName prints the name of the project.
+//
+// This comes from Config.Name.
 func PrintName(c cookoo.Context, p *cookoo.Params) (interface{}, cookoo.Interrupt) {
 	cfg := p.Get("conf", nil).(*Config)
 	fmt.Println(cfg.Name)
