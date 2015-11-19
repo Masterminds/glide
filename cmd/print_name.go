8af6d919ab745f4e6efdb8e9e8ab3b91a33cc171
Matt Farina <matt@mattfarina.com>
2015-11-19 16:10:45 -0500

    One more place for revised cfg

--- cmd/print_name.go
+++ cmd/print_name.go
@@ -4,14 +4,14 @@ import (
 	"fmt"
 
 	"github.com/Masterminds/cookoo"
-	"github.com/Masterminds/glide/yaml"
+	"github.com/Masterminds/glide/cfg"
 )
 
 // PrintName prints the name of the project.
 //
 // This comes from Config.Name.
 func PrintName(c cookoo.Context, p *cookoo.Params) (interface{}, cookoo.Interrupt) {
-	cfg := p.Get("conf", nil).(*yaml.Config)
-	fmt.Println(cfg.Name)
+	conf := p.Get("conf", nil).(*cfg.Config)
+	fmt.Println(conf.Name)
 	return nil, nil
 }
