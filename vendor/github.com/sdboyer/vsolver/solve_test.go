db4ef967c79b974e0915bf0db7954e9cf9eb5b76
Sam Boyer <tech@samboyer.org>
2016-05-17 23:40:29 -0400

    One more vsolver update

--- vendor/github.com/sdboyer/vsolver/solve_test.go
+++ vendor/github.com/sdboyer/vsolver/solve_test.go
@@ -1,6 +1,7 @@
 package vsolver
 
 import (
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -8,17 +9,23 @@ import (
 	"testing"
 )
 
+var fixtorun string
+
 // TODO regression test ensuring that locks with only revs for projects don't cause errors
+func init() {
+	flag.StringVar(&fixtorun, "vsolver.fix", "", "A single fixture to run in TestBasicSolves")
+}
 
 var stderrlog = log.New(os.Stderr, "", 0)
 
 func TestBasicSolves(t *testing.T) {
-	//solveAndBasicChecks(fixtures[8], t)
 	for _, fix := range fixtures {
-		solveAndBasicChecks(fix, t)
-		if testing.Verbose() {
-			// insert a line break between tests
-			stderrlog.Println("")
+		if fixtorun == "" || fixtorun == fix.n {
+			solveAndBasicChecks(fix, t)
+			if testing.Verbose() {
+				// insert a line break between tests
+				stderrlog.Println("")
+			}
 		}
 	}
 }
@@ -35,17 +42,21 @@ func solveAndBasicChecks(fix fixture, t *testing.T) (res Result, err error) {
 		ChangeAll: fix.changeall,
 	}
 
+	if fix.l != nil {
+		o.L = fix.l
+	}
+
 	if testing.Verbose() {
 		o.Trace = true
 	}
 
 	s := NewSolver(sm, stderrlog)
-
-	if fix.l != nil {
-		o.L = fix.l
-	}
-
 	res, err = s.Solve(o)
+
+	return fixtureSolveBasicChecks(fix, res, err, t)
+}
+
+func fixtureSolveBasicChecks(fix fixture, res Result, err error, t *testing.T) (Result, error) {
 	if err != nil {
 		if len(fix.errp) == 0 {
 			t.Errorf("(fixture: %q) Solver failed; error was type %T, text: %q", fix.n, err, err)
@@ -53,10 +64,10 @@ func solveAndBasicChecks(fix fixture, t *testing.T) (res Result, err error) {
 
 		switch fail := err.(type) {
 		case *BadOptsFailure:
-			t.Error("Unexpected bad opts failure solve error: %s", err)
+			t.Error("(fixture: %q) Unexpected bad opts failure solve error: %s", fix.n, err)
 		case *noVersionError:
 			if fix.errp[0] != string(fail.pn.LocalName) { // TODO identifierify
-				t.Errorf("Expected failure on project %s, but was on project %s", fail.pn.LocalName, fix.errp[0])
+				t.Errorf("(fixture: %q) Expected failure on project %s, but was on project %s", fix.n, fail.pn.LocalName, fix.errp[0])
 			}
 
 			ep := make(map[string]struct{})
@@ -79,7 +90,7 @@ func solveAndBasicChecks(fix fixture, t *testing.T) (res Result, err error) {
 				}
 			}
 			if len(extra) > 0 {
-				t.Errorf("Expected solve failures due to projects %s, but solve failures also arose from %s", strings.Join(fix.errp[1:], ", "), strings.Join(extra, ", "))
+				t.Errorf("(fixture: %q) Expected solve failures due to projects %s, but solve failures also arose from %s", fix.n, strings.Join(fix.errp[1:], ", "), strings.Join(extra, ", "))
 			}
 
 			for p, _ := range ep {
@@ -88,7 +99,7 @@ func solveAndBasicChecks(fix fixture, t *testing.T) (res Result, err error) {
 				}
 			}
 			if len(missing) > 0 {
-				t.Errorf("Expected solve failures due to projects %s, but %s had no failures", strings.Join(fix.errp[1:], ", "), strings.Join(missing, ", "))
+				t.Errorf("(fixture: %q) Expected solve failures due to projects %s, but %s had no failures", fix.n, strings.Join(fix.errp[1:], ", "), strings.Join(missing, ", "))
 			}
 
 		default:
@@ -140,7 +151,60 @@ func solveAndBasicChecks(fix fixture, t *testing.T) (res Result, err error) {
 		}
 	}
 
-	return
+	return res, err
+}
+
+// This tests that, when a root lock is underspecified (has only a version) we
+// don't allow a match on that version from a rev in the manifest. We may allow
+// this in the future, but disallow it for now because going from an immutable
+// requirement to a mutable lock automagically is a bad direction that could
+// produce weird side effects.
+func TestRootLockNoVersionPairMatching(t *testing.T) {
+	fix := fixture{
+		n: "does not pair bare revs in manifest with unpaired lock version",
+		ds: []depspec{
+			dsv("root 0.0.0", "foo *"), // foo's constraint rewritten below to foorev
+			dsv("foo 1.0.0", "bar 1.0.0"),
+			dsv("foo 1.0.1 foorev", "bar 1.0.1"),
+			dsv("foo 1.0.2 foorev", "bar 1.0.2"),
+			dsv("bar 1.0.0"),
+			dsv("bar 1.0.1"),
+			dsv("bar 1.0.2"),
+		},
+		l: mklock(
+			"foo 1.0.1",
+		),
+		r: mkresults(
+			"foo 1.0.2 foorev",
+			"bar 1.0.1",
+		),
+	}
+
+	pd := fix.ds[0].deps[0]
+	pd.Constraint = Revision("foorev")
+	fix.ds[0].deps[0] = pd
+
+	sm := newdepspecSM(fix.ds)
+
+	l2 := make(fixLock, 1)
+	copy(l2, fix.l)
+	l2[0].v = nil
+
+	o := SolveOpts{
+		Root: string(fix.ds[0].Name()),
+		N:    ProjectName(fix.ds[0].Name()),
+		M:    fix.ds[0],
+		L:    l2,
+	}
+
+	if testing.Verbose() {
+		o.Trace = true
+	}
+
+	s := NewSolver(sm, stderrlog)
+	res, err := s.Solve(o)
+
+	fixtureSolveBasicChecks(fix, res, err, t)
 }
 
 func getFailureCausingProjects(err error) (projs []string) {
