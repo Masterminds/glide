521573f0c9ccb8323093b12ad6fd44877858b4e0
Matt Farina <matt@mattfarina.com>
2017-09-25 10:46:57 -0400

    Update wizard spacing and messaging

--- action/config_wizard.go
+++ action/config_wizard.go
@@ -203,11 +203,11 @@ func wizardRemember() bool {
 func wizardAskRange(ver *semver.Version, d *cfg.Dependency) string {
 	vstr := ver.String()
 	msg.Info("The package %s appears to use semantic versions (http://semver.org).", d.Name)
-	msg.Info("Would you like to track the latest minor or patch releases (major.minor.patch) ?")
+	msg.Info("Would you like to track the latest minor or patch releases (major.minor.patch)?")
 	msg.Info("The choices are:")
-	msg.Info(" - tracking minor version releases would use '>= %s, < %d.0.0' ('^%s').", vstr, ver.Major()+1, vstr)
-	msg.Info(" - tracking patch version releases would use '>= %s, < %d.%d.0' ('~%s').", vstr, ver.Major(), ver.Minor()+1, vstr)
-	msg.Info(" - skip ranges\n")
+	msg.Info(" - Tracking minor version releases would use '>= %s, < %d.0.0' ('^%s')", vstr, ver.Major()+1, vstr)
+	msg.Info(" - Tracking patch version releases would use '>= %s, < %d.%d.0' ('~%s')", vstr, ver.Major(), ver.Minor()+1, vstr)
+	msg.Info(" - Skip using ranges\n")
 	msg.Info("For more information on Glide versions and ranges see https://glide.sh/docs/versions")
 	msg.Info("Minor (M), Patch (P), or Skip Ranges (S)?")
 
